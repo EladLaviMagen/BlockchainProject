cff046d1cab9908df7b24e25929f5c96e744b9a9
Elad Magen <eladmagen10@gmail.com>
2023-11-12 14:10:04 +0200

    Initial connection and creation of network created (test 1)

--- P2P/P2P/Source.cpp
+++ P2P/P2P/Source.cpp
@@ -12,8 +12,8 @@ int main()
 	std::string input = "";
 	std::cout << "Would you like to open a network?\n";
 	std::cin >> input;
-	SOCKET m_serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (m_serverSocket == INVALID_SOCKET)
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET)
 		throw std::exception(__FUNCTION__ " - socket");
 	if (input == "a")
 	{
@@ -28,17 +28,17 @@ int main()
 		sa.sin_addr.s_addr = INADDR_ANY;    // when there are few ip's for the machine. We will use always "INADDR_ANY"
 
 		// Connects between the socket and the configuration (port and etc..)
-		if (bind(m_serverSocket, (struct sockaddr*)&sa, sizeof(sa)) == SOCKET_ERROR)
+		if (bind(sock, (struct sockaddr*)&sa, sizeof(sa)) == SOCKET_ERROR)
 			throw std::exception(__FUNCTION__ " - bind");
 
 		// Start listening for incoming requests of clients
-		if (listen(m_serverSocket, SOMAXCONN) == SOCKET_ERROR)
+		if (listen(sock, SOMAXCONN) == SOCKET_ERROR)
 			throw std::exception(__FUNCTION__ " - listen");
 		std::cout << "Listening on port " << PORT << std::endl;
 
 		// this accepts the client and create a specific socket from server to this client
 			// the process will not continue until a client connects to the server
-		SOCKET client_socket = accept(m_serverSocket, NULL, NULL);
+		SOCKET client_socket = accept(sock, NULL, NULL);
 		if (client_socket == INVALID_SOCKET)
 			throw std::exception(__FUNCTION__);
 
@@ -52,7 +52,15 @@ int main()
 	}
 	else
 	{
-		connect()
+		input = "";
+		std::cout << "enter IP of network\n";
+		std::cin >> input;
+		struct sockaddr_in sa = { 0 };
+		sa.sin_port = htons(PORT); // port that server will listen for 
+		sa.sin_family = AF_INET;   // must be AF_INET 
+		sa.sin_addr.s_addr = inet_addr(input.c_str());
+		connect(sock, (struct sockaddr*)&sa, sizeof(sa));
+
 	}
 	
     return 0;
