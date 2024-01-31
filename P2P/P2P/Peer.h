0d41dbe708dd394241bb2e0ececc71ed825af722
Elad Magen <eladmagen10@gmail.com>
2024-01-31 22:25:08 +0200

    P2P changes in preparation for merge

---
+++ P2P/P2P/Peer.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <exception>
+#include <iostream>
+#include <string>
+#include <WinSock2.h>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <thread>
+#include "Helper.h"
+#include <map>
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 5078
+class Peer
+{
+public:
+	static std::map<int, SOCKET> users;
+	static WSADATA wsaData;
+	//Declarations
+	static void recieveMessageAndHandle(SOCKET sc);
+	static void connectMoreUsers(SOCKET listenSocket, int port);
+private:
+	
+
+
+};
+
