5971810eb999776f0871202abdb02742f1919219
Elad Magen <eladmagen10@gmail.com>
2024-02-10 15:58:40 +0200

    small fix

--- Blockchain/Blockchain/Blockchain.h
+++ Blockchain/Blockchain/Blockchain.h
@@ -11,11 +11,10 @@ public:
 	~Blockchain();
 	std::string toString();
 	Blockchain(std::string chainInfo);
-	int getCoinsOf(std::string user);
+	float getCoinsOf(std::string user);
 	void update(std::string newData);
 	bool addBlock(Block* newBlock, std::string);
 	void operator()();
-	bool operator==(Blockchain& other);
 	
 private:
 	std::map<std::string, Block*> chain;
